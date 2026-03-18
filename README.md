7d648c5215fc983cf61a09794c73316ea2db142f
Nick Davies <n.g.davies@gmail.com>
2026-03-18 17:12:35 +0000

    Initial commit

---
+++ README.md
@@ -0,0 +1,25 @@
+
+# modeller
+
+<!-- badges: start -->
+<!-- badges: end -->
+
+The goal of modeller is to ...
+
+## Installation
+
+You can install the development version of modeller like so:
+
+``` r
+# FILL THIS IN! HOW CAN PEOPLE INSTALL YOUR DEV PACKAGE?
+```
+
+## Example
+
+This is a basic example which shows you how to solve a common problem:
+
+``` r
+library(modeller)
+## basic example code
+```
+
---
+++ local/workflow.R
@@ -0,0 +1,67 @@
+# INITIAL SETUP
+
+# Add 'local' directory
+dir.create("./local")
+usethis::write_union("./.Rbuildignore", "^local$")
+
+# Readme and docs
+usethis::use_readme_md() # or rmd
+usethis::use_news_md()
+usethis::use_roxygen_md()
+usethis::use_package_doc()
+usethis::use_mit_license()
+usethis::use_cran_comments()
+devtools::document() # delete NAMESPACE before first time
+
+# Set up git
+usethis::git_vaccinate()
+usethis::use_git()
+usethis::use_github()
+usethis::use_github_action()
+
+# Web site
+usethis::use_pkgdown_github_pages()
+
+# Testing
+usethis::use_testthat()
+usethis::use_coverage()
+
+
+
+# ADDING ELEMENTS
+# Import package
+usethis::use_package("rlang")
+usethis::use_package("elixir")
+usethis::use_package("luajr")
+
+# Add vignette
+usethis::use_vignette("qrst")
+
+
+# BUILD CYCLE
+devtools::document()
+devtools::build_vignettes()
+# devtools::build_readme()
+# pkgdown::build_site()
+
+devtools::build(vignettes = TRUE)
+devtools::install(build_vignettes = TRUE)
+
+
+# RELEASE CYCLE
+devtools::check(remote = TRUE, manual = TRUE)
+devtools::check_win_devel()
+
+usethis::use_version('minor') # patch, minor, or major
+
+# CRAN
+# Revise cran-comments.md!
+devtools::submit_cran()
+
+# After accepted
+git push
+usethis::use_github_release()
+usethis::use_dev_version()
+git push
+Finish blog post, share on social media, etc.
+Add link to blog post in pkgdown news menu
